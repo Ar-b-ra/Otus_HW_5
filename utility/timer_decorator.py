886bed58b054e2f294f6b45f9c5fe66accfacd19
Roman Arbuzov <lpandlipmbizkit@yandex.ru>
2023-11-11 18:50:46 +0300

    refactor: немного переписан декоратор времени выполнения сортировки

--- utility/timer_decorator.py
+++ utility/timer_decorator.py
@@ -1,15 +1,16 @@
 import time
+from datetime import datetime
 
 from utility.custom_logger import root_logger
 
 
 def timer(func):
     def wrapper(*args, **kwargs):
-        start_time = time.time()
+        start_time = datetime.now()
         result = func(*args, **kwargs)
-        end_time = time.time()
+        end_time = datetime.now()
         execution_time = end_time - start_time
-        root_logger.info(f"Время выполнения функции {func.__name__}: {execution_time} секунд")
+        root_logger.info(f"Время выполнения функции {func.__name__}: {execution_time}")
         return result
 
     return wrapper
